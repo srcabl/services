3449873825cefc3d03301d69123fdcced831a644
Michael Kero <michael.kero@gmail.com>
2021-01-29 12:08:27 -0500

    init

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/srcabl/services
 
 go 1.15
+
+require (
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/srcabl/protos v0.1.0
+	google.golang.org/protobuf v1.25.0
+)
--- pkg/db/mysql/client.go
+++ pkg/db/mysql/client.go
@@ -1,1 +1,16 @@
 package mysql
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/srcabl/services/pkg/config"
+)
+
+type Client struct {
+	db sql.DB
+}
+
+func New(config *config.Environment) (*Client, error) {
+
+}
